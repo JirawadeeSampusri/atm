f55ba56b26e02d9dc2e84fccedf18a79f8ee306c
Jirawadee Sampusri <jirawadeelovely@hotmail.com>
2020-12-19 00:37:46 +0700

    edit main and add html

--- src/main/java/th/ac/ku/atm/service/BankAccountService.java
+++ src/main/java/th/ac/ku/atm/service/BankAccountService.java
@@ -3,15 +3,36 @@ package th.ac.ku.atm.service;
 import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.stereotype.Service;
 import th.ac.ku.atm.model.BankAccount;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.http.*;
 
 import javax.annotation.PostConstruct;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
 
 @Service
 public class BankAccountService {
 
     private List<BankAccount> accountList;
+    private RestTemplate restTemplate;
+
+
+    public BankAccountService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public List<BankAccount> getCustomerBankAccount(int customerId) {
+        String url = "http://localhost:8091/api/bankaccount/customer/" +
+                customerId;
+        ResponseEntity<BankAccount[]> response =
+                restTemplate.getForEntity(url, BankAccount[].class);
+
+        BankAccount[] accounts = response.getBody();
+
+        return Arrays.asList(accounts);
+    }
 
     @PostConstruct
     public void postConstruct() {
